b6aef61c8d2e3f9f325b88859f58c862c03ab41c
CALeyva <carlos.leyva@upr.edu>
2021-03-10 14:10:46 -0400

    Added map reset feature after respawning. Phase1.1

--- src/Game/States/GameState.cpp
+++ src/Game/States/GameState.cpp
@@ -38,6 +38,7 @@ void GameState::keyReleased(int key){
 void GameState::reset(){
 	setFinished(false);
 	setNextState("");
+	this->map = MapBuilder().createMap(mapImage);
 }
 
 int GameState::getFinalScore(){
